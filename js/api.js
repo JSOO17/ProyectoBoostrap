6b6eba22a2169e07863b4af75a7c17d0eba4ea4f
JSOO17 <57716971+JSOO17@users.noreply.github.com>
2020-05-09 00:47:11 -0500

    agregamos todo

---
+++ js/api.js
@@ -0,0 +1,39 @@
+
+
+document.addEventListener('DOMContentLoaded', init);
+function init(){
+	let url = 'https://thesimpsonsquoteapi.glitch.me/quotes?count=7';
+	fetch(url)
+	.then(response => response.json())
+	.then(content => {
+		console.log(content);
+		let img_header = document.getElementsByClassName('img_header');
+		let caracter = document.getElementsByClassName('caracter');
+		let quote  = document.getElementsByClassName('quote');
+		let i = 0;
+		do{
+			img_header[i].src = content[i].image;
+			caracter[i].innerText = content[i].character;
+			quote[i].innerText = content[i].quote;
+			i++;
+		}while(i<=1);
+
+	});
+	fetch(url)
+	.then(response => response.json())
+	.then(content => {
+		let caracter2 = document.getElementsByClassName('caracter2');
+		let quote2  = document.getElementsByClassName('quote2');
+		console.log(quote2[0]);
+		i = 0;
+		do{
+			caracter2[i].innerText = content[i].character;
+			quote2[i].innerText = content[i].quote;
+			console.log(i);
+			i++;
+		}while(i<=7);
+
+	});
+	
+}
+
